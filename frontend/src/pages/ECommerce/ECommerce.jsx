2c43e288281a918b9953d815768e4282d10d70e8
Herschel TUIL <herschel.tuil@gmail.com>
2022-10-26 11:31:39 +0200

    Added the header

--- frontend/src/pages/ECommerce/ECommerce.jsx
+++ frontend/src/pages/ECommerce/ECommerce.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./ECommerce.css";
+import Header from "./Header";
 
 export default function ECommerce() {
-  return <div>E-commerce</div>;
+  return (
+    <div>
+      <Header />
+    </div>);
 }
