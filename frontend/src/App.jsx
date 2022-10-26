0fd50bac8f0def931020790435e157863ec17ea1
Johanna <johanna.stoicanescu@gmail.com>
2022-10-26 10:46:03 +0100

    structure balise + quelques elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import ECommerce from "@pages/ECommerce/ECommerce";
+import Restaurant from "@pages/Restaurant/Restaurant"
 import Home from "@pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
 
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/e-commerce" element={<ECommerce />} />
-        {/* <Route path="restaurant" element={<Restaurant />} />
-        <Route path="startup" element={<Startup />} /> */}
+        <Route path="restaurant" element={<Restaurant />} />
+        {/*<Route path="startup" element={<Startup />} /> */}
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Restaurant/Restaurant.jsx
@@ -0,0 +1,99 @@
+import "./Restaurant.css";
+
+export default function Restaurant() {
+    return <div>
+        <header>
+            <img src="/frontend/src/assets/cameleonWithe.png" alt="logo du site" />
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Lorem</li>
+                    <li>Ipsum</li>
+                    <li>Dolor</li>
+                    <li>Sitamet</li>
+                </ul>
+                <img src="" alt="" />
+            </nav>
+        </header>
+        <main>
+            <section>
+                <div>
+                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ratione eveniet in quos aliquam provident sed consequatur
+                        saepe facilis voluptas autem?</p>
+                    <button>Lorem Order</button>
+                </div>
+
+                <img src="" alt="" />
+            </section>
+            <section>
+                <h3>Lorem Ipsum Dolor</h3>
+                <button>Element</button>
+                <button>Element</button>
+                <button>Element</button>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h6>Item Name</h6>
+                    <p>$ lorem USD</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque quia voluptatibus voluptas enim explicabo, eius tenetur ut.</p>
+                    <input type="text" />
+                    <button></button>
+                </div>
+                <button>Lorem Ipsum</button>
+            </section>
+            <section>
+                <img src="" alt="" />
+                <h3></h3>
+                <p></p>
+                <button></button>
+            </section>
+        </main>
+        <footer>
+            <img src="" alt="" />
+            <div>
+                <h3>Lorem ipsum dolor sit, amet consectetur.</h3>
+            </div>
+        </footer>
+    </div>;
+}
